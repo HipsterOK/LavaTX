19a7559353038386d09163c7b5050a3e72af7290
HispterOK <stero141@gmail.com>
2026-02-28 13:57:31 +0500

    Add LAVA ONE support and Cross-OS Build Action

---
+++ companion/build/radio/src/stamp.h
@@ -0,0 +1,8 @@
+#define DATE    "2026-02-28"
+#define TIME    "13:48:45"
+#define VERSION "1.0.0"
+
+#define VERSION_MAJOR     1
+#define VERSION_MINOR     0
+#define VERSION_REVISION  0
+#define GIT_STR           "100b01ff"
---
+++ companion/build/radio/src/tests/location.h
@@ -0,0 +1,2 @@
+#define TESTS_PATH        "/home/stero141/Projects/MicrocontrollersProjects/opentx-release-2.3.15/radio/src/tests"
+#define TESTS_BUILD_PATH  "/home/stero141/Projects/MicrocontrollersProjects/opentx-release-2.3.15/companion/build/radio/src/tests"
